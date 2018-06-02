57a276aa1f699e6ebaf1d21b0b95ff321ca9e9d7
alisaghari <alisaghari.awd@gmail.com>
2018-06-02 20:33:52 +0430

    valibal on git

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
 use Illuminate\Http\Request;
 use App\User;
 class AdminController extends Controller
 {
-
+    public function login(){
+        return view("admin.tlogin");
+    }
+    public function loginAdmin(Request $request){
+        session_start();
+        $users=Admin::where("name", $request->input('username'))->where("password", $request->input('password'))->get();
+        foreach ($users as $user) {
+            $_SESSION["admin_id"] = $user["id"];
+            $_SESSION["username"] = $user["username"] ;
+        }
+        return back();
+    }
     public function userList()
     {
       $users = User::all();
